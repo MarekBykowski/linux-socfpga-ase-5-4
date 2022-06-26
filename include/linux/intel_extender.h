4fc409c7c3e64d12dfd39768b4a8be5016d30570
Marek Bykowski <marek.bykowski@gmail.com>
2022-06-26 11:46:23 +0100

    extender trace call: Fix for a case with no arguments
    Before the fix calling extender_trace_call() without the extra
    arguments, namely only with the frame number, eg. extender_trace_call(3),
    resulted in with a compilation error, now it is fixed.
    
    Note that before and now calling the extender_trace_call() with the extra
    args, eg. extender_trace_call(3, "phys_addr %pa %zx\n", &phys_addr, size)
    has been fine.
    
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- include/linux/intel_extender.h
+++ include/linux/intel_extender.h
@@ -8,9 +8,21 @@
 
 #ifdef DEBUG
 #include <asm/stacktrace.h> /* For struct stackframe */
-#define extender_trace_call(frames, fmt, ...)	\
+
+/* args counting based on include/linux/arm-smccc.h by Marc Zyngier */
+#define _count_args(_0, _1, _2, _3, _4, _5, _6, _7, x, ...) x
+#define count_args(...)						\
+	_count_args(__VA_ARGS__, 7, 6, 5, 4, 3, 2, 1, 0)
+
+#define extender_trace_call(frames, ...)	\
+	do {		\
+		if (count_args(__VA_ARGS__) == 1)	\
+			_extender_trace_call_fmt(frames, "");	\
+		_extender_trace_call_fmt(frames, __VA_ARGS__);	\
+	} while (0)
+
+#define _extender_trace_call_fmt(frames, fmt, ...)	\
 	do {	\
-		/*dump_backtrace(NULL, NULL);*/	\
 		struct stackframe frame;	\
 		struct task_struct *tsk = current;	\
 		int i;	\
@@ -20,16 +32,16 @@
 				(unsigned long)_THIS_IP_);	\
 	\
 		pr_debug("extender trace call:\n");	\
-		pr_debug("1: %pS args: " fmt, (void *)frame.pc, ##__VA_ARGS__);	\
+		pr_debug("1: %pS " fmt, (void *)frame.pc, ##__VA_ARGS__);	\
 		unwind_frame(tsk, &frame);	\
 	\
 		for (i = 2; i <= frames; i++) {	\
 			pr_debug("%d: %pS\n", i, (void *)frame.pc);	\
 			unwind_frame(tsk, &frame);	\
 		}	\
-	} while(0)
+	} while (0)
 #else
-#define extender_trace_call(frames, fmt, ...)	do {} while(0)
+#define extender_trace_call(frames, fmt, ...)	do {} while (0)
 #endif
 
 /*
